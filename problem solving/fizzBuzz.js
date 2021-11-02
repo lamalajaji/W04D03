c95f2c2c89438c5407cb025d6b113ab868bdd202
lamalajaji <Lama.Mohammed.elq.js@tuwaiq.edu.sa>
2021-11-02 09:22:54 +0300

    solve 1

---
+++ problem solving/fizzBuzz.js
@@ -0,0 +1,19 @@
+
+const fizBuzz = (n)=>{
+
+    if (n % 3 == 0){
+        return ("fizz")
+    }
+    else if (n % 5 ==0 ){
+        return ("Buzz")
+    }
+    else if (n % 3 == 0 && n % 5 ==0){
+        return ("fizzBuzz")
+    }
+    else{
+        for (let i = 1; i <= n; i++) {
+            console.log(i);;
+            
+        }
+    }
+}
